476193255f6a4fc9c8773cd86643bcc68709cafc
MarSely-GH <mihail.martynov.2011@gmail.com>
2026-08-20 16:05:51 +0500

    Update ИоПиВОР cache to v7

--- iopivor/sw.js
+++ iopivor/sw.js
@@ -1,5 +1,5 @@
-const CACHE='iopivor-simple-v6';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./app.js','./rules1.js','./rules2.js','./rules3.js','./rules4.js','./rules5.js','./rules6.js','./rules7.js'];
+const CACHE='iopivor-simple-v7';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./app.js','./rules1.js','./rules2.js','./rules3.js','./rules4.js','./rules5.js','./rules6.js','./rules7.js','./rules8.js'];
 self.addEventListener('install',e=>{self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;if(e.request.mode==='navigate'){e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put('./index.html',c));return r;}).catch(()=>caches.match('./index.html')));return;}e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r;}).catch(()=>caches.match(e.request)));});
