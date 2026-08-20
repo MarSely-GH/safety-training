567504cbb55d2cc4e60dece1de7194b2e47bd65b
MarSely-GH <mihail.martynov.2011@gmail.com>
2026-08-20 16:55:54 +0500

    Fix AutoPoisk offline cache assets

--- autopoisk/sw.js
+++ autopoisk/sw.js
@@ -1,5 +1,5 @@
 const CACHE="autopoisk-v13-pwa1";
-const ASSETS=["./","./index.html","./manifest.json","./icon.svg","./AutoPoisk_base_170.json"];
+const ASSETS=["./","./index.html","./manifest.json","./icon.svg"];
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}));self.skipWaiting();});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener("fetch",event=>{
