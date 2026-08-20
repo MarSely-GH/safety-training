7813c1f4dc09a8dae53bfe5b77fc8100f9ab07b0
MarSely-GH <mihail.martynov.2011@gmail.com>
2026-08-20 16:55:16 +0500

    Add AutoPoisk service worker

---
+++ autopoisk/sw.js
@@ -0,0 +1,10 @@
+const CACHE="autopoisk-v13-pwa1";
+const ASSETS=["./","./index.html","./manifest.json","./icon.svg","./AutoPoisk_base_170.json"];
+self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}));self.skipWaiting();});
+self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener("fetch",event=>{
+  const req=event.request;
+  if(req.method!=="GET")return;
+  if(new URL(req.url).origin!==self.location.origin)return;
+  event.respondWith(fetch(req).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(req,copy)).catch(()=>{});return resp;}).catch(()=>caches.match(req).then(r=>r||caches.match("./index.html"))));
+});
