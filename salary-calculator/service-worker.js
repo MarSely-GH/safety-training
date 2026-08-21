f478a7668ab5846915477d1094dd5b2210f4a869
MarSely-GH <mihail.martynov.2011@gmail.com>
2026-08-21 18:17:04 +0500

    Refresh salary calculator cache

--- salary-calculator/service-worker.js
+++ salary-calculator/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='salary-calculator-shell-v3-voice-fix';
+const CACHE='salary-calculator-v5-day-night';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()]))});
