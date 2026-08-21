36719ce9294c5e66266cca529af1a8d2e6a13e50
MarSely-GH <mihail.martynov.2011@gmail.com>
2026-08-21 11:34:09 +0500

    Refresh salary calculator cache

--- salary-calculator/service-worker.js
+++ salary-calculator/service-worker.js
@@ -1,10 +1,13 @@
-const CACHE='salary-calculator-shell-v1';
+const CACHE='salary-calculator-shell-v2';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([
+  self.clients.claim(),
+  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+])));
 self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET')return;
-  e.respondWith(fetch(e.request).then(resp=>{
+  e.respondWith(fetch(e.request,{cache:'no-store'}).then(resp=>{
     const copy=resp.clone();
     caches.open(CACHE).then(c=>c.put(e.request,copy));
     return resp;
